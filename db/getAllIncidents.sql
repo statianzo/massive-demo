dfafd1d13ba67fbe065699b0bd6d30bc70b9589e
Jason Staten <jason.staten@domo.com>
2018-07-12 12:38:28 -0600

    wpr-39 final

---
+++ db/getAllIncidents.sql
@@ -0,0 +1,4 @@
+select incidents.id, state, injuries.name as injury, affectedareas.name as affectedarea, causes.name as cause from incidents
+join injuries on incidents.injuryid = injuries.id
+join affectedareas on injuries.affectedareaid = affectedareas.id
+join causes on causes.id = incidents.causeid
--- server.js
+++ server.js
@@ -1,23 +1,53 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const massive = require('massive');
+
+const connectionString = 'postgres://YOUR__CONNECTION__STRING';
 
 const app = express();
 app.use(bodyParser.json());
 
 const port = 3000;
 
+app.set('cool', 'jason');
+
 app.get('/', (req, res) => {
-  res.send('massive-demo');
+  const db = req.app.get('db');
+  db.getAllInjuries().then(injuries => {
+    res.send(injuries);
+  });
 });
 
 app.get('/incidents', (req, res) => {
-  res.send([]);
+  const db = req.app.get('db');
+  const state = req.query.state;
+
+  if (state) {
+    db.getIncidentsByState({state: state}).then(incidents => {
+      res.send(incidents);
+    });
+  }
+  else {
+    db.getAllIncidents().then(incidents => {
+      res.send(incidents);
+    });
+  }
+
 });
 
 app.post('/incidents', (req, res) => {
-  res.send({id: 123});
+  const db = req.app.get('db');
+  const incident = req.body;
+  db.createIncident(incident).then(result => {
+    res.send(result[0]);
+  });
 });
 
-app.listen(port, () => {
-  console.log('Started server on port', port);
+massive(connectionString).then(connection => {
+  app.set('db', connection);
+
+  app.listen(port, () => {
+    console.log('Started server on port', port);
+  });
 });
+
