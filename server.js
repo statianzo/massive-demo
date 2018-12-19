f2ca412be888f1bd095478bcb40dedcf2a11e526
Jason Staten <jstaten07@gmail.com>
2018-12-19 09:19:44 -0700

    wpr-45 final

--- server.js
+++ server.js
@@ -1,23 +1,48 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const massive = require('massive');
+const connectionString = 'postgres://YOUR_CONNECTION_STRING?ssl=true';
 const app = express();
 app.use(bodyParser.json());
 
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('massive-demo');
+  const db = req.app.get('db');
+  db.getAllInjuries().then(injuries => {
+    res.send(injuries);
+  })
 });
 
 app.get('/incidents', (req, res) => {
-  res.send([]);
+  const db = req.app.get('db');
+  const state = req.query.state;
+  if (state) {
+    db.getIncidentsByState({ usState: state }).then(incidents => {
+      res.send(incidents);
+    })
+  }
+  else {
+    db.getAllIncidents().then(incidents => {
+      res.send(incidents);
+    })
+  }
 });
 
 app.post('/incidents', (req, res) => {
-  res.send({id: 123});
+  const incident = req.body;
+  const db = req.app.get('db');
+  db.createIncident(incident).then(result => {
+    res.send(result[0]);
+  })
+    .catch(err => {
+      res.status(400).send(err.message)
+    })
 });
 
-app.listen(port, () => {
-  console.log('Started server on port', port);
-});
+massive(connectionString).then(connection => {
+  app.set('db', connection);
+  app.listen(port, () => {
+    console.log('Started server on port', port);
+  });
+})
